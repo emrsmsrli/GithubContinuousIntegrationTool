2b091547551c2700002e6bb64d67acc390808f56
Emre <emrsmsrli1@gmail.com>
2018-04-11 16:47:23 +0300

    new format function

--- app/services/package.scala
+++ app/services/package.scala
@@ -1,8 +1,10 @@
 
 package object services {
-    def formatGithubHookUrl(username: String, repository: String): String = {
-        s"https://api.github.com/repos/$username/$repository/hooks"
-    }
+    def formatGithubHookUrl(username: String, repository: String)
+        = s"https://api.github.com/repos/$username/$repository/hooks"
+
+    def formatGithubZipballUrl(username: String, repository: String)
+        = s"https://api.github.com/repos/$username/$repository/zipball/master"
 
     def formatPubSubUrl(subscriberId: Long)
         = s"https://f9146eee.ngrok.io/push/$subscriberId"
